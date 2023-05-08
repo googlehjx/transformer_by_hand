271238399ac4c9a0b45fd7af5c3e49151d64a6ca
jxhe <jxhe@mail.ustc.edu.cn>
2023-05-08 12:43:22 +0800

    添加了greedy_decoder

---
+++ model/infer.py
@@ -0,0 +1,33 @@
+import os
+import sys
+root = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(root)
+import torch
+
+from data.my_data import tgt_vocab
+
+def greedy_decoder(model, enc_input, start_symbol):
+    """
+    For simplicity, a Greedy Decoder is Beam search when K=1. This is necessary for inference as we don't know the
+    target sequence input. Therefore we try to generate the target input word by word, then feed it into the transformer.
+    Starting Reference: http://nlp.seas.harvard.edu/2018/04/03/attention.html#greedy-decoding
+    :param model: Transformer Model
+    :param enc_input: The encoder input
+    :param start_symbol: The start symbol. In this example it is 'S' which corresponds to index 4
+    :return: The target input
+    """
+    enc_outputs, enc_self_attns = model.encoder(enc_input)
+    dec_input = torch.zeros(1, 0).type_as(enc_input.data)
+    terminal = False
+    next_symbol = start_symbol
+    while not terminal:         
+        dec_input = torch.cat([dec_input.detach(),torch.tensor([[next_symbol]],dtype=enc_input.dtype).cuda()],-1)
+        dec_outputs, _, _ = model.decoder(dec_input, enc_input, enc_outputs)
+        projected = model.projection(dec_outputs)
+        prob = projected.squeeze(0).max(dim=-1, keepdim=False)[1]
+        next_word = prob.data[-1]
+        next_symbol = next_word
+        if next_symbol == tgt_vocab["."]:
+            terminal = True
+        print(next_word)            
+    return dec_input
