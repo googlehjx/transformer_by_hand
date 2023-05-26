6e4e042d6a864f5e505211000225a5a73cc8f6e7
He Jinxin <jxhe@mail.ustc.edu.cn>
2023-05-26 08:07:13 +0800

    Update my_vocab.py
    添加数据处理代码

--- data/my_vocab.py
+++ data/my_vocab.py
@@ -6,6 +6,33 @@ sentences = [
         ['ich mochte ein bier P', 'S i want a beer .', 'i want a beer . E'],
         ['ich mochte ein cola P', 'S i want a coke .', 'i want a coke . E']
 ]
+import json
+import pickle
+f = open("translation2019zh_train.json")
+english_sentences = []
+chinese_sentences = []
+for line in f.readlines():
+        data = json.loads(line)
+        english_sentences.append(data["english"])
+        chinese_sentences.append(data["chinese"])
+assert(len(english_sentences) == len(chinese_sentences))
+from bpe import Encoder
+en_encoder = Encoder(32000, pct_bpe=0.88)
+en_encoder.fit(english_sentences)
+zh_encoder = Encoder(32000, pct_bpe=0.88)
+zh_encoder.fit(chinese_sentences)
+# example = "Slowly and not without struggle, America began to listen."
+# print(encoder.tokenize(example))
+en_segment_sentences = [en_encoder.tokenize(sent) for sent in english_sentences ]
+zh_segment_sentences = [zh_encoder.tokenize(sent) for sent in chinese_sentences]
+file = open("zh_segment_sentences", "wb")
+pickle.dump(zh_segment_sentences, file)
+file.close()
+file = open("en_segment_sentences", "wb")
+pickle.dump(en_segment_sentences, file)
+file.close()    
+
+
 
 # Padding Should be Zero
 src_vocab = {'P' : 0, 'ich' : 1, 'mochte' : 2, 'ein' : 3, 'bier' : 4, 'cola' : 5}
